bc8b954f35a720c425adc930aa73b9d0c7bae89e
Rufus2018 <143760870+Rufus2018@users.noreply.github.com>
2025-01-21 19:41:14 -0500

    Completed FruitSalad Example
    Example of OOP

---
+++ FruitSaladLibrary/FruitSaladLibrary/Strawberry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitSaladLibrary
+{
+    public class Strawberry : Fruit
+    {
+        public bool IsItGoodForJam {  get; set; }
+        public int SweetnessLevel { get; set; }
+        public Strawberry(string color,double weight,bool isOrganic, bool IsItGoodForJam, int SweetnessLevel) :
+            base("Strawberry",color,weight, isOrganic)
+        {
+            SweetnessLevel = SweetnessLevel;
+            IsItGoodForJam = IsItGoodForJam; 
+        }
+        //implementation of the abstract Describe method
+        public override string Describe()
+        {
+            string jamLabel = IsItGoodForJam ? "Good for jam!" : "Not ideal for jam";
+            return $"{base.ToString()}, Sweetness Level: {SweetnessLevel}, {jamLabel}";
+        }
+    }
+}
